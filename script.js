c3821d8d3eca7ece4452e787d0f912737baa67f2
Maliha-Moon <malihamoon05@gmail.com>
2024-10-04 12:30:50 +0600

    javascript display on web page

---
+++ script.js
@@ -0,0 +1,4 @@
+//alert("Welcome to Javasript"); //display massage in alert box
+document.write("Welcome to Javasript"); //write specific text directly to the document
+console.log("Hello world!");
+
