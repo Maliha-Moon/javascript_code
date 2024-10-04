2117ee4f7f4d02c1f3a431d8a53f0083c9fa5fc5
Maliha-Moon <malihamoon05@gmail.com>
2024-10-04 12:31:49 +0600

    js practise object data types

---
+++ objectTypes.js
@@ -0,0 +1,12 @@
+let Profile = {
+    name: 'Maliha Moon',
+    isFollow: true,
+    posts: 195,
+    followers: 569000,
+    following: 4,
+    bio: 'student,CSE,RPSU'
+};
+
+console.log(Profile);
+console.log(typeof(Profile));
+console.log(typeof(Profile["isFollow"]));
