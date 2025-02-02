31ed3855a6a9067510d86f40ddfe74fa5d7900f3
Maliha Rahman Moon <malihamoon05@gmail.com>
2025-02-02 22:51:59 +0600

    Add js

---
+++ WeatherApp/script.js
@@ -0,0 +1,26 @@
+// 0f81a598d2f2ceddd0a2241e85196c89
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+const apiKeys = "0f81a598d2f2ceddd0a2241e85196c89"
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchInput = document.querySelector('.search-input');
+const searchBtn = document.querySelector('.search-btn');
+const cityName = document.querySelector('.city');
+const temp = document.querySelector('.temp');
+const humidity = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
+
+async function checkWeather(city) {
+    let fetchAPI = await fetch(apiURL + city + `&appid=${apiKeys}`);
+    let data = await fetchAPI.json();
+     console.log(data);
+    temp.innerHTML = Math.round(data.main.temp) + "°c";
+    cityName.innerHTML = data.name;
+    humidity.innerHTML = data.main.humidity + "%";
+    wind.innerHTML = data.wind.speed + " km/h";
+}
+
+searchBtn.addEventListener('click',()=>{
+    checkWeather(searchInput.value); 
+});
